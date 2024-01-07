c7dd8fa39f1137b91daded88aee449e5493fb2e9
LefterisIkaria <lolopap21@gmail.com>
2024-01-07 17:40:36 +0200

    Add daisyui library and update UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import ThemeController from "./components/ThemeController";
+
 function App() {
   return (
     <>
-      <div className="text-blue-500">App</div>
+      <header className="container flex items-center justify-between py-2">
+        <h1 className="text-2xl font-black">Todon't</h1>
+        <ThemeController />
+      </header>
+
+      <main className="container">
+        <form className="flex items-center justify-between py-2 gap-4">
+          <input
+            type="text"
+            className="input input-bordered w-full"
+            placeholder="What do you need to do?"
+          />
+          <button type="submit" className="btn btn-neutral">
+            Add Task
+          </button>
+        </form>
+
+        <div></div>
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
 };
